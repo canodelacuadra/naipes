563271d018a41797f597f450f28701b52fccdf63
José Antonio Cano <canodelacuadra@gmail.com>
2017-03-16 13:14:23 +0100

    ponemos estilo a la baraja

--- mostrarBaraja.js
+++ mostrarBaraja.js
@@ -1,18 +1,13 @@
-
+function mostrarCartas(){
 //version 2
-for(var i=0; i<baraja.length;i++){
-        
-      
+for(var i=0; i<baraja.length;i++){     
  var carta="<div class='carta'>";
-    carta += "<span class='numcart'>";
-    carta += i+1;
-    carta += "</span>";
-     carta += "<span class='palo'>";
-    carta += baraja[i].palo;
-     carta += "</span>";
-     carta += "<span class='numero'>";
-    carta += baraja[i].numero;
+
+     carta += "<span class='fuente'>";
+    carta += baraja[i].font;
      carta += "</span>";
     carta +="</div>";
+    
     document.getElementById('tapete').innerHTML+=carta;        
     }
+}
